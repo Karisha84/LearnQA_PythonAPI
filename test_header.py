0d5d59cc93a7b4b6865a727497103e4b6b5d99e9
Karina84 <sineglazka-84@mail.ru>
2023-04-15 16:05:35 +0300

    fix EX12

--- test_header.py
+++ test_header.py
@@ -4,6 +4,9 @@ import requests
 class TestCookie:
     def test_cookie(self):
         response = requests.get("https://playground.learnqa.ru/api/homework_header")
-        print(response.text)
+        print(response.headers)
+        headers_value = response.headers.get("x-secret-homework-header")
+        print(headers_value)
 
-        assert 'success":"!"' in response.text, "There is no text"
+        assert 'x-secret-homework-header' in response.headers, "There is no header in response"
+        assert headers_value == "Some secret value", "Wrong headers value"
