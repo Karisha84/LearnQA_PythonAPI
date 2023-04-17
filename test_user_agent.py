5c0143e56f386d75e9ccf8d925c15282f5bc73ad
Karina84 <sineglazka-84@mail.ru>
2023-04-17 21:31:08 +0300

    Ex13

---
+++ test_user_agent.py
@@ -0,0 +1,52 @@
+import pytest
+import requests
+
+
+class TestGetUserAgent:
+    data = [
+        (
+            "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 "
+            "(KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+            "Mobile",
+            "No",
+            "Android"
+        ),
+        (
+            "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) "
+            "CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1",
+            "Mobile",
+            "Chrome",
+            "iOS"
+        ),
+        (
+            "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+            "Googlebot",
+            "Unknown",
+            "Unknown"
+        ),
+        (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77"
+            "Safari/537.36 Edg/91.0.100.0",
+            "Web",
+            "Chrome",
+            "No"
+        ),
+        (
+            "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) "
+            "Version/13.0.3 Mobile/15E148 Safari/604.1",
+            "Mobile",
+            "No",
+            "iPhone"
+        )
+    ]
+
+    @pytest.mark.parametrize('agent, platform, browser, device', data)
+    def test_get_user_agent(self, agent, platform, browser, device):
+        url = "https://playground.learnqa.ru/ajax/api/user_agent_check"
+        data = {"user-agent": agent}
+
+        response = requests.get(url, headers=data)
+        parsed_response = response.json()
+        assert parsed_response["platform"] == platform, f"{agent} has wrong platform"
+        assert parsed_response["browser"] == browser, f"{agent} has wrong browser"
+        assert parsed_response["device"] == device, f"{agent} has wrong device"
